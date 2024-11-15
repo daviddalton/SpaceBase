cb3c163c369563563e8d9998375a32f55a8255a9
David Dalton <daviddalton@Davids-MacBook-Pro.local>
2024-11-14 22:40:50 -0500

    clean up readme and add safety to the commands for single objects

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-SpaceBase hits the [SpaceX API] -- https://docs.spacexdata.com/#58df29dc-5839-42fc-9540-2f572fd13438
+## SpaceBase 
+***
+###### SpaceBase is a Python scripting application that hits the [SpaceX API](https://docs.spacexdata.com/#58df29dc-5839-42fc-9540-2f572fd13438) for Launch Data
 
+```
+launches                  fetch all launches
+launch {flight_number}    fetch one launch
+rockets                   fetch all rockets
+rocket {rocket_id}        fetch one rocket
+launch_sites              fetch all launch sites
+launch_site {site_id}     fetch one launch site
+```
 
-"launches"                  fetch all launches
-"launch {flight_number}"    fetch one launch
-"rockets"                   fetch all rockets
-"rocket {rocket_id}"        fetch one rocket
-"launch_sites"              fetch all launch sites
-"launch_site {site_id}"     fetch one launch site
--- main.py
+++ main.py
@@ -26,32 +26,32 @@ def initialize_launch_site_data():
 
 
 def interpret_user_input(user_input):
-    if user_input == "help":
-        print('"launches"                  fetch all launches')
-        print('"launch {flight_number}"    fetch one launch')
-        print('"rockets"                   fetch all rockets')
-        print('"rocket {rocket_id}"        fetch one rocket')
-        print('"launch_sites"              fetch all launch sites')
-        print('"launch_site {site_id}"     fetch one launch site')
-    elif user_input == "launches":
+    if user_input.strip() == "help":
+        print('launches                  fetch all launches')
+        print('launch {flight_number}    fetch one launch')
+        print('rockets                   fetch all rockets')
+        print('rocket {rocket_id}        fetch one rocket')
+        print('launch_sites              fetch all launch sites')
+        print('launch_site {site_id}     fetch one launch site')
+    elif user_input.strip() == "launches":
         for launch in launch_data:
             print(launch.__str__())
     elif user_input.__contains__("launch "):
         flight_number = user_input.split(" ")[1].strip()
-        print(next((x for x in launch_data if str(x.flight_number) == str(flight_number)), None).__str__())
-    elif user_input == "rockets":
+        print(next((x for x in launch_data if str(x.flight_number) == str(flight_number)), "Launch not found.").__str__())
+    elif user_input.strip() == "rockets":
         for rocket in rocket_data:
             print(rocket.__str__())
     elif user_input.__contains__("rocket "):
         rocket_id = user_input.split(" ")[1].strip()
-        print(next((x for x in rocket_data if str(x.rocket_id) == str(rocket_id)), None).__str__())
-    elif user_input == "launch_sites":
+        print(next((x for x in rocket_data if str(x.rocket_id) == str(rocket_id)), "Rocket not found.").__str__())
+    elif user_input.strip() == "launch_sites":
         for launch_site in launch_site_data:
             print(launch_site.__str__())
     elif user_input.__contains__("launch_site "):
         launch_site_id = user_input.split(" ")[1].strip()
-        print(next((x for x in launch_site_data if str(x.site_id) == str(launch_site_id)), None).__str__())
-    elif user_input == "exit":
+        print(next((x for x in launch_site_data if str(x.site_id) == str(launch_site_id)), "Launch Site not found.").__str__())
+    elif user_input.strip() == "exit":
         return False
     else:
         return True
